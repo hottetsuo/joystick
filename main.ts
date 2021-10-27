c9e807ab9b0f8bbd06be077a18a90e9dba004dff
hottetsuo <91411166+hottetsuo@users.noreply.github.com>
2021-10-27 20:42:11 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    if (pins.analogReadPin(AnalogPin.P1) > 560) {
+        led.plot(0, 0)
+        led.unplot(0, 2)
+        led.unplot(0, 4)
+    } else if (pins.analogReadPin(AnalogPin.P1) < 460) {
+        led.unplot(0, 0)
+        led.unplot(0, 2)
+        led.plot(0, 4)
+    } else {
+        led.unplot(0, 0)
+        led.plot(0, 2)
+        led.unplot(0, 4)
+    }
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    if (pins.analogReadPin(AnalogPin.P1) > 560) {
+        led.plot(4, 0)
+        led.unplot(4, 2)
+        led.unplot(4, 4)
+    } else if (pins.analogReadPin(AnalogPin.P1) < 460) {
+        led.unplot(4, 0)
+        led.unplot(4, 2)
+        led.plot(4, 4)
+    } else {
+        led.unplot(4, 0)
+        led.plot(4, 2)
+        led.unplot(4, 4)
+    }
+})
