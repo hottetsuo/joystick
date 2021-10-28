d77861fdb69248a1f829811a6ee219161e9362ab
hottetsuo <91411166+hottetsuo@users.noreply.github.com>
2021-10-28 19:56:33 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,32 +1,32 @@
 basic.forever(function () {
+    basic.pause(100)
     pins.digitalWritePin(DigitalPin.P2, 0)
-    if (pins.analogReadPin(AnalogPin.P1) > 560) {
-        led.plot(0, 0)
+    if (pins.analogReadPin(AnalogPin.P0) > 560) {
+        led.plot(2, 2)
+        led.unplot(1, 2)
         led.unplot(0, 2)
-        led.unplot(0, 4)
-    } else if (pins.analogReadPin(AnalogPin.P1) < 460) {
-        led.unplot(0, 0)
-        led.unplot(0, 2)
-        led.plot(0, 4)
-    } else {
-        led.unplot(0, 0)
+    } else if (pins.analogReadPin(AnalogPin.P0) < 460) {
+        led.unplot(2, 2)
+        led.unplot(1, 2)
         led.plot(0, 2)
-        led.unplot(0, 4)
+    } else {
+        led.unplot(2, 2)
+        led.plot(1, 2)
+        led.unplot(0, 2)
     }
-})
-basic.forever(function () {
+    basic.pause(100)
     pins.digitalWritePin(DigitalPin.P2, 1)
     if (pins.analogReadPin(AnalogPin.P1) > 560) {
-        led.plot(4, 0)
-        led.unplot(4, 2)
-        led.unplot(4, 4)
+        led.plot(3, 1)
+        led.unplot(3, 2)
+        led.unplot(3, 3)
     } else if (pins.analogReadPin(AnalogPin.P1) < 460) {
-        led.unplot(4, 0)
-        led.unplot(4, 2)
-        led.plot(4, 4)
+        led.unplot(3, 1)
+        led.unplot(3, 2)
+        led.plot(3, 3)
     } else {
-        led.unplot(4, 0)
-        led.plot(4, 2)
-        led.unplot(4, 4)
+        led.unplot(3, 1)
+        led.plot(3, 2)
+        led.unplot(3, 3)
     }
 })
